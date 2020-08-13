acd38f5ed05aa1127e547a3592b07a0300f86ec8
Carlos <carlosspy@gmail.com>
2020-08-13 13:21:45 -0300

    static

--- bank/settings.py
+++ bank/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 AUTHENTICATION_BACKENDS = [
     "django.contrib.auth.backends.ModelBackend",
